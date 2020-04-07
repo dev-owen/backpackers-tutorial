76da35822a77779ee7ee2bdaf5733e205f481b9d
wonjongoh94 <jgyuity1289@gmail.com>
2020-04-07 13:41:55 +0900

    ESlint issue handling (4 spaces indent in vue -> script tag)

--- client/src/Home.vue
+++ client/src/Home.vue
@@ -1,7 +1,54 @@
 <template>
     <div>
-        <hr />
-        <div class="list-unstyled" v-for="message in messages" :key="message._id">
+        <form @submit.prevent="addMessage" class="mb-3">
+            <div v-if="error" class="alert alert-dismissible alert-warning">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <h4 class="alert-heading">Error!</h4>
+                <p class="mb-0">{{ error }}</p>
+            </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input
+                    v-model="message.username"
+                    type="text"
+                    class="form-control"
+                    id="username"
+                    required
+                />
+            </div>
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input
+                    v-model="message.subject"
+                    type="text"
+                    class="form-control"
+                    id="subject"
+                    placeholder="Enter a subject"
+                    required
+                />
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea
+                    v-model="message.message"
+                    class="form-control"
+                    id="message"
+                    rows="3"
+                ></textarea>
+            </div>
+            <div class="form-group">
+                <label for="imageURL">Image URL</label>
+                <input
+                    v-model="message.imageURL"
+                    type="url"
+                    class="form-control"
+                    id="imageURL"
+                    placeholder="Enter URL to an image"
+                />
+            </div>
+            <button type="submit" class="btn btn-primary">Add Message</button>
+        </form>
+        <div class="list-unstyled" v-for="message in reversedMessages" :key="message._id">
             <li class="media">
                 <img
                     v-if="message.imageURL"
@@ -26,20 +73,53 @@
 const API_URL = 'http://localhost:4000/messages';
 
 export default {
-  name: 'home',
-  data: () => ({
-    error: '',
-    messages: [],
-  }),
-
-  mounted() {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((result) => {
-        this.messages = result;
-      });
-  },
-  methods: {},
+    name: 'home',
+    data: () => ({
+        error: '',
+        messages: [],
+        message: {
+            username: 'Enter a screen name',
+            subject: '',
+            message: '',
+            imageURL: '',
+        },
+    }),
+    computed: {
+        reversedMessages() {
+            return this.messages.slice().reverse();
+        },
+    },
+    mounted() {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then((result) => {
+                this.messages = result;
+            });
+    },
+    methods: {
+        addMessage() {
+            console.log(this.message);
+            fetch(API_URL, {
+                method: 'POST',
+                body: JSON.stringify(this.message),
+                headers: {
+                    'content-type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((result) => {
+                    if (result.details) {
+                        // there was an error...
+                        const error = result.details.map((detail) => detail.message).join('. ');
+                        this.error = error;
+                    } else {
+                        this.error = '';
+                        this.showMessageForm = false;
+                        this.messages.push(result);
+                    }
+                });
+        },
+    },
 };
 </script>
 
